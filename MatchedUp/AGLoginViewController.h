7e1000b9888a0443e8eb34010d5e31ff61d713e8
AGart <A_Gartenberg@yahoo.com>
2014-01-30 16:15:04 -0500

    First commit

---
+++ MatchedUp/AGLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  AGLoginViewController.h
+//  MatchedUp
+//
+//  Created by Albee Gartenberg on 1/30/14.
+//  Copyright (c) 2014 Albee Gartenberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AGLoginViewController : UIViewController
+
+@end
---
+++ MatchedUp/AGLoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  AGLoginViewController.m
+//  MatchedUp
+//
+//  Created by Albee Gartenberg on 1/30/14.
+//  Copyright (c) 2014 Albee Gartenberg. All rights reserved.
+//
+
+#import "AGLoginViewController.h"
+
+@interface AGLoginViewController ()
+
+@end
+
+@implementation AGLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
